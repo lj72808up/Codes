377732930be94f8171bb650ca8ad149f35ce95cc
lj72808up <lj72808up@163.com>
2023-12-25 09:50:12 +0800

    rename

--- SPI_Test/src/main/java/com2/test/proxy/jdk/Animal.java
+++
@@ -1,7 +0,0 @@
-package com2.test.proxy.jdk;
-
-public interface Animal {
-    void eat();
-    void sayHello(String word);
-    void go();
-}
--- SPI_Test/src/main/java/com2/test/proxy/jdk/multiLevelProxy/linkList/MyMethodInvocation.java
+++
@@ -1,9 +0,0 @@
-package com2.test.proxy.jdk.multiLevelProxy.linkList;
-
-public interface MyMethodInvocation {
-
-    /**
-     * 进入拦截器链中的下一个拦截器，驱动责任链模式向前运行
-     */
-    Object proceed() throws Throwable;
-}
